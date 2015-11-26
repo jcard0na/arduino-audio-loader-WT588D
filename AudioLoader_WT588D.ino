96754a393efdeda9bb318678b554062ffb818bbc
Javier Cardona <javier@muchi.co>
2015-11-26 09:22:18 -0800

    Code formatting and documentation

--- AudioLoader_WT588D.ino
+++ AudioLoader_WT588D.ino
@@ -3,68 +3,70 @@
 // Modified by purgedsoul for uploading bin file to WTD588-16P sound module.
 // Use at your own risk!
 
+#include <Esplora.h>
 #include <Adafruit_TinyFlash.h>
 #include <SPI.h>
 
+#define CHIP_SELECT_PIN   7
+
 void setup() {
-	Adafruit_TinyFlash flash;
-
-	uint8_t            buffer[256];
-
-	int                c, index      = 0, 
-			   rmdr          = 0,
-			   numpages      = 0,
-			   page          = 0;
-
-	uint32_t           capacity,
-			   address       = 0,
-			   bytesExpected = 0x7FFFFFFF,
-			   bytesReceived = 0,
-			   filesize=0;
-
-	Serial.begin(57600);
-	Serial.println("HELLO");      // ACK to host
-
-	capacity = flash.begin();
-	Serial.println(capacity);     // Chip size to host
-
-
-
-	while(!Serial.find("ERASE")); // Wait for ERASE command from host
-	if(!flash.eraseChip()) {
-		Serial.println("ERROR");
-	}
-
-	while(!Serial.find("CAPACITY")); 
-	filesize = Serial.parseInt();
-
-	numpages = filesize / 256;
-	rmdr = filesize % 256;
-
-	if(rmdr != 0) numpages = numpages + 1;
-
-	Serial.println("READY");      // ACK to host
-
-	for(;;) {
-		// Buffer data until a full page is ready or last packet arrives.
-		if((c = Serial.read()) >= 0) {
-			buffer[index] = c;
-			index=index+1;
-			if((index >= sizeof(buffer))) {
-				if(flash.writePage(address, buffer)) {
-
-					Serial.print('.');
-				} else {
-					Serial.print('X');
-				}
-
-				if(page >= numpages) break; //(address >= capacity) || 
-				index = 0;
-				page = page + 1;
-				address = address + sizeof(buffer);
-			}
-		} 
-	} 
+  Serial.begin(9600);
 }
 
-void loop() { }
+void loop() {
+  Adafruit_TinyFlash flash = Adafruit_TinyFlash(CHIP_SELECT_PIN);
+
+  uint8_t            buffer[256];
+
+  int                c, index      = 0,
+         rmdr          = 0,
+         numpages      = 0,
+         page          = 0;
+
+  uint32_t           capacity,
+         address       = 0,
+         bytesExpected = 0x7FFFFFFF,
+         bytesReceived = 0,
+         filesize=0;
+
+  pinMode(CHIP_SELECT_PIN, OUTPUT);
+  Serial.println("HELLO");      // ACK to host
+  capacity = flash.begin();
+  Serial.println(capacity);     // Chip size to host
+
+  while(!Serial.find("ERASE")); // Wait for ERASE command from host
+  if(!flash.eraseChip()) {
+    Serial.println("ERROR");
+  }
+
+  while(!Serial.find("CAPACITY"));
+  filesize = Serial.parseInt();
+
+  numpages = filesize / 256;
+  rmdr = filesize % 256;
+
+  if(rmdr != 0) numpages = numpages + 1;
+
+  Serial.println("READY");      // ACK to host
+
+  for(;;) {
+    // Buffer data until a full page is ready or last packet arrives.
+    if((c = Serial.read()) >= 0) {
+      buffer[index] = c;
+      index=index+1;
+      if((index >= sizeof(buffer))) {
+        if(flash.writePage(address, buffer)) {
+
+          Serial.print('.');
+        } else {
+          Serial.print('X');
+        }
+
+        if(page >= numpages) break; //(address >= capacity) ||
+        index = 0;
+        page = page + 1;
+        address = address + sizeof(buffer);
+      }
+    }
+  }
+}
--- ProcessingAudioTransfer/ProcessingAudioTransfer.pde
+++ ProcessingAudioTransfer/ProcessingAudioTransfer.pde
@@ -1,5 +1,5 @@
 // WT588D .bin file transfer.  Runs in conjunction with
-// AudioLoader_WT588D.ino sketch on Arduino 
+// AudioLoader_WT588D.ino sketch on Arduino
 // Processing sketch based off AdaFruit AudioXfer sketch
 // Use at your own risk
 
@@ -26,11 +26,11 @@ void setup() {
 
   // Locate Arduino running AudioLoader sketch.
   // Try each serial port, checking for ACK after opening.
-  
+
   println("Scanning serial ports...");
    for(String portname : Serial.list()) {
     try {
-      portname = "COM13";                        // bypass scan
+      portname = "/dev/cu.usbmodem1411";                        // bypass scan
       port = new Serial(this, portname, 57600);
     } catch (Exception e) {                      // Port in use, etc.
       continue;
@@ -49,7 +49,7 @@ void setup() {
   if(port != null) {                              // Find one?
     if(((s        = readLine())                != null)
       && ((capacity = Integer.parseInt(s.trim())) > 0)) {
-      println("Found Arduino w/" + capacity + " byte flash chip."); 
+      println("Found Arduino w/" + capacity + " byte flash chip.");
       selectInput("Select a file to process:", "fileSelected");
     } else {
       println("Arduino failed to initialize flash memory.");
@@ -58,7 +58,7 @@ void setup() {
   } else {
     println("Could not find connected Arduino running WT588DAudioLoader sketch.");
     done = true;
-  } 
+  }
 }
 
 void fileSelected(File f) {
@@ -69,41 +69,41 @@ void fileSelected(File f) {
     byte input[] = loadBytes(f.getAbsolutePath());
 
     int filesize = input.length;
-    
-    
+
+
       port.write("ERASE");                          // Issue erase command now, process audio while it works
 
       println("Processing file...\n");
-                 
+
       print("Erasing flash...");                    // Really just waiting for the erase ACK
-      
+
       String s, cap;
       int c;
-      
+
       cap = "CAPACITY" + filesize;
       port.write(cap);
-                  
+
       while(((s = readLine()) == null) || (s.contains("READY") == false));
-      
+
       print("\nFilesize: ");
       println(cap);
       print("OK\nWriting...\n");
       // Instead of just write(output), it's done per-byte so we
       // can echo progress dots from the Arduino to the console.
       // Eventually may want to make it re-write error sectors.
-     
+
       for(int i=0; i<filesize; i++) {
         //a = input[i]; //& 0xff;
         port.write(input[i]);
-              
+
         if((c = port.read()) >= 0) {print((char)c);}
       }
       println("done!");
-     
-    } 
- 
+
+    }
+
    done = true;
-   
+
 }
 
 void draw() {
